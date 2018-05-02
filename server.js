eac5dfdcea0183bc64e144bbecef546dccf7b3e0
Brandon Lewis <dotsony@gmail.com>
2018-05-02 16:05:56 -0700

    Licenses Everywhere!

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 #! /usr/bin/node
 
+/* MIT License
+ *
+ * Copyright (c) 2018 Carbine Coffee
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 const  http = require('http'),
        url = require('url'),
        uuid4 = require('uuid/v4'),
@@ -7,7 +30,7 @@ const  http = require('http'),
        port = process.env.HTTP_PORT || 80,
        ws_server = new ws.Server({port});
 
-class ProxyConnection {
+class RelaySession {
   constructor (origin, token) {
     this.origin = origin;
     this.token = token;
